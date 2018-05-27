e47cf9a895bbf08cf350533bc783f81c71b7d324
Shoggomo <andy-hot@web.de>
2018-05-27 03:27:36 +0200

    Added source to db entry

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ set_name = "dashboard"
 
 
 def main():
+    print("Connecting to database...")
     redis_db = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+    print("Connecting to reddit...")
     reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+
+    print("Now crawling...")
 
     for e in reddit.subreddit('all').stream.submissions():
         if search_term in str.lower(e.title):
             print("%s: %s - %s, %s" % (e.created_utc, e.title, e.author, e.selftext))
             key = 'red_%s' % e.id
-            redis_db.hsetnx(set_name, key, {"title": e.title, "author": e.author.name, "text": e.selftext, "timestamp": e.created_utc})
+            redis_db.hsetnx(set_name, key, {"source": "reddit", ""title": e.title, "author": e.author.name, "text": e.selftext, "timestamp": e.created_utc})
 
 
 if __name__ == "__main__":
